fbddef6c89ad1d566ff85a105a88e19f4f8f6489
jurgen <jurgen@qasedo>
2020-02-28 14:25:53 +0200

    hello

---
+++ src/source/Calculator.java
@@ -0,0 +1,9 @@
+package source;
+
+public class Calculator {
+
+	public int sum(int i, int j) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
---
+++ src/test/testcode.java
@@ -0,0 +1,40 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import source.Calculator;
+
+class testcode {
+	static Calculator calc;
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		calc = new Calculator();
+	}
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {	
+	}
+	@Test
+	void test_BothArePositive() {
+		assertTrue(calc.sum(10, 10) > 0);
+	}
+	@Test
+	void test_BothAreNegative() {
+		assertTrue(calc.sum(-10, -10) < 0);
+	}
+	@Test
+	void test_X_GreaterThan_Y_absolute() {
+		assertTrue(calc.sum(20, -10) > 0);
+	}
+	@Test
+	void test_Y_GreaterThan_X_absolute() {
+		assertTrue(calc.sum(10, -20) < 0);
+	}
+	@Test
+	void test_BothEqualsWithOppositeSigns() {
+		assertTrue(calc.sum(10, -10) == 0);
+	}
+}
