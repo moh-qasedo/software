e0c52197ee130515bc7f6a41515508033a9e0b29
jurgen <jurgen@qasedo>
2020-02-28 14:51:49 +0200

    3rd edition

--- src/test/testcode.java
+++ src/test/testcode.java
@@ -1,7 +1,7 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
